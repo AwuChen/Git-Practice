b58ed77ffc8863744a5546cb8e633304214e0f56
AlexChen <alexchentaiwan@gmail.com>
2016-07-17 00:36:53 +0800

    I made some changes to the ehlloworld class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("HEHEHEHEHE IM BREAKING IT");
+		System.out.println("HIM BREAKING IT");
+		System.out.println("This is AWUUUUUUUUUUUUUUUUUUUU!!!!! IM a BEAST");
 	}
 }
