bc3ee8967a3d09a29d407cb1bed3cfac3c2f4cc5
Eric Price <wunderkammer123@gmail.com>
2016-07-16 11:44:32 -0500

    edited println

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("HEHEHEHEHE IM BREAKING IT");
-		System.out.println("8====D~~~~");
+		System.out.println("Test String");
 		System.out.println("HIM BREAKING IT");
 		System.out.println("This is AWUUUUUUUUUUUUUUUUUUUU!!!!! IM a BEAST");
 	}
