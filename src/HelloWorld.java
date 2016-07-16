3960050763138e22c186dc864288a484ee6152a4
AlexChen <alexchentaiwan@gmail.com>
2016-07-17 00:47:16 +0800

    made some more changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,10 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("This is AWU 2nfd edit modified this line changed the order");
 		System.out.println("HEHEHEHEHE IM BREAKING IT");
 		System.out.println("8====D~~~~");
 		System.out.println("HIM BREAKING IT");
-		System.out.println("This is AWUUUUUUUUUUUUUUUUUUUU!!!!! IM a BEAST");
+		
 	}
 }
