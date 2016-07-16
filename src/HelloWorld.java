b0b6154922e5d41d2e6804ee77e6af561df61dbc
AlexChen <alexchentaiwan@gmail.com>
2016-07-17 00:52:14 +0800

    i think it's working too

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,6 +8,8 @@ public class HelloWorld {
 		System.out.println("HIM BREAKING IT");
 		System.out.println("Hey, I think this is working.");
 		
+		System.out.println("yeah I think so too!!");
+		
 		
 		
 		System.out.println("Hey Eric when do you want to goto your friends marching band thing?");
